1265e761f4e216a41305711d9d1fe3f5f0db2716
jeremioho <120092957+jeremioho@users.noreply.github.com>
2022-12-11 13:26:32 +0000

    ye

--- animation/script.js
+++ animation/script.js
@@ -27,3 +27,19 @@ const backdrop = document.querySelector('.backdrop');
 trigger.addEventListener('click', () => nav.classList.add('open-nav'));
 backdrop.addEventListener('click', () => nav.classList.remove('open-nav'));
 
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+
+function printInput(){
+  output.innerHTML = input.value;
+}
+
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("button").click();
+  }
+});
